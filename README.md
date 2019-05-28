25bd7746a8121f5c5ad0a02b70411d40f90a7e32
danielhorsley <danielhorsley@users.noreply.github.com>
2019-05-28 21:28:46 +0100

    this is my first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # lambdata library
-making a library
+making a library with some (semi!)useful function
+
+the class is calls frame_ops and is instended for operations on data frames
+
+the first function turns a column into a percentage of the total value of the column
+you can choose one column or a list of columns or all columns
+
+the second function normalizes a column
--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name="lambdata-danh",
-    version="0.0.1",
+    version="0.0.2",
     author="danh",
     description="A collection of Data Science Helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/soycode/lambdata",
+    url="https://github.com/danh/lambdata",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=REQUIRED,
