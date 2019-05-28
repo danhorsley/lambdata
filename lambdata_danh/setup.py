2d590ddeeecfd079b96aeec27d35a9dd45946760
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-05-28 20:05:35 +0100

    set up file

---
+++ lambdata_danh/setup.py
@@ -0,0 +1,27 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED=['numpy','python']
+
+with open('README.md','r') as fh:
+    LONG_DESCRIPTION=fh.read()
+
+setuptools.setup(
+    name="lambdata-danh",
+    version="0.0.1",
+    author="danh",
+    description="A collection of Data Science Helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/soycode/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
