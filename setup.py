0eb75d8ae7ffdbcf81433c11a03e5ba3f5912537
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-05-28 21:42:03 +0100

    Update setup.py
    small edit version and correct url

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name="lambdata-danh",
-    version="0.0.2",
+    version="0.0.3",
     author="danh",
     description="A collection of Data Science Helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/danh/lambdata",
+    url="https://github.com/danhorsley/lambdata",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=REQUIRED,
