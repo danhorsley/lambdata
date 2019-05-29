18a757dee028df45bdcc84ac21cd03f14ed43017
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-05-29 23:21:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name="lambdata_danh",
-    version="0.0.4",
+    version="0.0.5",
     author="danh",
     description="A collection of Data Science Helper functions",
     long_description=LONG_DESCRIPTION,
